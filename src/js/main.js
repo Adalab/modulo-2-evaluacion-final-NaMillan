7bd0e2bf68f8e933c2d5d529994dc57ed3ad3b60
NaMillan <nazarethmillan@gmail.com>
2024-01-19 09:58:27 +0100

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+console.log('>> Ready :)');
+const search = document.querySelector('.js-seacher');
+const btnSearch = document.querySelector('.js-button-search');
+const btnReset = document.querySelector('.js-button-reset');
+const listImgResult= document.querySelector('.js-img-result');
+const listImgFavorites= document.querySelector('.js-img-favorites');
+
+
+
