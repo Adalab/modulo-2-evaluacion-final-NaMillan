c5353e7327f099269b136a7a44377f57005e698a
NaMillan <nazarethmillan@gmail.com>
2024-01-21 21:02:00 +0100

    add favorites

--- src/js/main.js
+++ src/js/main.js
@@ -4,21 +4,22 @@ const search = document.querySelector('.js-seacher');
 const btnSearch = document.querySelector('.js-button-search');
 const btnReset = document.querySelector('.js-button-reset');
 const listResult= document.querySelector('.js-container');
-//const listTitleResult= document.querySelector('.js-title-img');
-//const listImgFavorites= document.querySelector('.js-img-favorites');
-//const listTitleResultFavorites= document.querySelector('.js-title-img-favorite');
+const listFavorites= document.querySelector('.js-container-fav');
 
-let titleslist = [];
+
+
+let titlesList = [];
 let initialUrl='https://api.jikan.moe/v4/anime?q='
 let url = initialUrl + 'naruto';
+let favoritesTitles = [];
 
 function getDataApi (){
     fetch (url)
     .then ((response => response.json()))
     .then ((data) =>{
-    titleslist=data.data;
-    console.log (titleslist);
-    renderTitles (titleslist);
+    titlesList=data.data;
+    renderTitles (titlesList);
+    renderFavorites(favoritesTitles);
     });
 }
 //getDataApi();
@@ -28,20 +29,23 @@ const handleInput = (event) => {
     const valueInput = search.value.toLowerCase().replace(/\s+/g, '-');
     url = initialUrl + valueInput;
     console.log(url);
-    getDataApi();
-  
-
+   getDataApi();
 };
 
 btnSearch.addEventListener('click', handleInput);
 
-const renderTitles = (titleslist) => {
+
+const renderTitles = (titlesList) => {
     listResult.innerHTML = '';
-    titleslist.forEach((title) => {
+  
+        titlesList.forEach((title) => {
         const titleContainer = document.createElement('div');
         const imgElement = document.createElement('img');
         const titleElement = document.createElement('h3');
         
+        titleContainer.classList.add('js-title-selected');
+        titleContainer.id = `${title.mal_id}`;
+
         if (title.images.jpg.image_url !== '') {
         imgElement.src = title.images.jpg.image_url;
         } else {
@@ -55,5 +59,49 @@ const renderTitles = (titleslist) => {
 
         listResult.appendChild(titleContainer);
     });
+    listenerTitles();
 };
 
+function handleAddFavorite(event) {
+    const clickedTitleId = parseInt(event.currentTarget.id);
+    const foundTitle = titlesList.find(title => title.mal_id === parseInt(clickedTitleId));
+    const indexTitleFav=favoritesTitles.findIndex((title) => title.mal_id === clickedTitleId);
+     if (indexTitleFav === -1){
+        favoritesTitles.push(foundTitle); 
+      
+    }
+    renderFavorites(favoritesTitles);
+}
+
+function listenerTitles(){
+    const allTitles = document.querySelectorAll('.js-title-selected');
+
+    for (const oneTitle of allTitles){
+        oneTitle.addEventListener('click', handleAddFavorite);
+
+    }
+}
+
+function renderFavorites(favoritesTitles) {
+    listFavorites.innerHTML = '';
+    favoritesTitles.forEach((favorite) => {
+        const favoriteContainer = document.createElement('div');
+        const imgElement = document.createElement('img');
+        const titleElement = document.createElement('h3');
+
+
+        if (favorite.images.jpg.image_url !== '') {
+            imgElement.src = favorite.images.jpg.image_url;
+            } else {
+            imgElement.src = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
+        }
+
+        favoriteContainer.classList.add('js-favorite-selected');
+        favoriteContainer.id = `${favorite.mal_id}`;
+
+        favoriteContainer.appendChild(imgElement);
+        favoriteContainer.appendChild(titleElement);
+
+        listFavorites.appendChild(favoriteContainer);
+    });
+}
